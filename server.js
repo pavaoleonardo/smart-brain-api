17aac7f3767519afaacce445da972b860f988c7b
PeacockCo <pavaoleonardo@hotmail.com>
2019-01-15 16:56:17 +0100

    add port

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.post('/imageurl', auth.requireAuth, (req, res) => {
   image.handleApiCall(req, res)
 })
 
-app.listen(process.env.PORT ||  3000, () => {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
