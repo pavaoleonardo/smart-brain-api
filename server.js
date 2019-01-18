fc0f6b07452e79f38f2df17d3b106e6105547f59
PeacockCo <pavaoleonardo@hotmail.com>
2019-01-18 13:24:22 +0100

    changing ports

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.post('/imageurl', auth.requireAuth, (req, res) => {
   image.handleApiCall(req, res)
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT}`);
-})
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
