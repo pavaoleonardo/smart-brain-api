4e62570b2ec9c2e0e33354e056a51f86b175e051
PeacockCo <pavaoleonardo@hotmail.com>
2019-01-18 13:35:14 +0100

    ports

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.post('/imageurl', auth.requireAuth, (req, res) => {
   image.handleApiCall(req, res)
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT}`);
-})
+server.listen(config.port, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
